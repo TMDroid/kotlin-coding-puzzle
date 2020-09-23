6344eb2d0d3d80f47299287e679fcfa6b920ba56
Daniel Bejan <dannyb97danny@gmail.com>
2020-09-23 13:46:17 +0300

    solution for Factorial

--- app/src/test/java/com/igorwojda/integer/factorial/Factorial.kt
+++ app/src/test/java/com/igorwojda/integer/factorial/Factorial.kt
@@ -4,7 +4,7 @@ import org.amshove.kluent.shouldEqual
 import org.junit.Test
 
 private fun factorial(n: Int): Int {
-    TODO("not implemented")
+    return (n downTo 1).fold(1) { acc, i -> acc * i}
 }
 
 class RecursiveFactorial {
