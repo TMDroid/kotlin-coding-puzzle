0737bd7c9bbf23bd01c67e97dfef0f3549d4f368
Daniel Bejan <daniel.bejan@cobaltsign.com>
2020-09-23 12:32:14 +0300

    solution for ContainsRange

--- app/src/test/java/com/igorwojda/range/containsrange/ContainsRange.kt
+++ app/src/test/java/com/igorwojda/range/containsrange/ContainsRange.kt
@@ -4,7 +4,7 @@ import org.amshove.kluent.shouldEqual
 import org.junit.Test
 
 private fun containsRange(range1: IntRange, range2: IntRange): Boolean {
-    TODO("not implemented")
+    return range2.all { range1.contains(it) }
 }
 
 class ContainsRangeTest {
