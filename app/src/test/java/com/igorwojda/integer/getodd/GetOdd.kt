e3472c4cb109e2bb58f6ab6af9fbc060cea63dd0
Daniel Bejan <daniel.bejan@cobaltsign.com>
2020-09-23 11:44:20 +0300

    solution for GetOdd

--- app/src/test/java/com/igorwojda/integer/getodd/GetOdd.kt
+++ app/src/test/java/com/igorwojda/integer/getodd/GetOdd.kt
@@ -4,7 +4,7 @@ import org.amshove.kluent.shouldEqual
 import org.junit.Test
 
 private fun filterOdd(list: List<Int>): List<Int> {
-    TODO("not implemented")
+    return list.filter { it % 2 == 1 }
 }
 
 class GetOddNumbersTest {
