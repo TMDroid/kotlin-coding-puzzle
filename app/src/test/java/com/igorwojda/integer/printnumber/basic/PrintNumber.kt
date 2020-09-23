7b120a2c8ba6a8faccd4a96a91a7e2094b990bd5
Daniel Bejan <daniel.bejan@cobaltsign.com>
2020-09-23 13:28:40 +0300

    solution for PrintNumber

--- app/src/test/java/com/igorwojda/integer/printnumber/basic/PrintNumber.kt
+++ app/src/test/java/com/igorwojda/integer/printnumber/basic/PrintNumber.kt
@@ -4,7 +4,7 @@ import org.amshove.kluent.shouldEqual
 import org.junit.Test
 
 private fun printNumber(n: Int): List<Int> {
-    TODO("not implemented")
+    return (n downTo 1).toList()
 }
 
 class RecursivePrintNumber {
