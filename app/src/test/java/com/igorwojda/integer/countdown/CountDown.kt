b17297e4086a0146eec0f34d1828cab115efb672
Daniel Bejan <daniel.bejan@cobaltsign.com>
2020-09-23 12:58:52 +0300

    solution for CountDown

--- app/src/test/java/com/igorwojda/integer/countdown/CountDown.kt
+++ app/src/test/java/com/igorwojda/integer/countdown/CountDown.kt
@@ -4,7 +4,7 @@ import org.amshove.kluent.shouldEqual
 import org.junit.Test
 
 private fun countDown(n: Int): List<Int> {
-    TODO("not implemented")
+    return (n downTo 0).toList()
 }
 
 class CountDownTest {
